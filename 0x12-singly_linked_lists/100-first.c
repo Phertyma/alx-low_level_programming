7dc7777facc76a7e29e77c871114bc285b0d3619
Fateema Shu'ayb <fatimashuaib6@gmail.com>
2022-10-20 14:30:53 -0700

    advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * main - function executed before main
  * Return: no return.
  */
-void __attribute__((constructor)) main()
+void __attribute__((constructor));
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
